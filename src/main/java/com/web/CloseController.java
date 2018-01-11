2fd23a380fbd5ff191601e649af8a033dbd464c4
lenziten <lenziten@hotmail.com>
2018-01-11 14:10:07 +0800

    commit

--- src/main/java/com/web/CloseController.java
+++ src/main/java/com/web/CloseController.java
@@ -41,7 +41,7 @@ public class CloseController {
     }
     
     @RequestMapping("/shutDown")
-	public String shutDownBoot(int returnCode){
+	public String shutDownBoot(){
     	int result = SpringApplication.exit(appContext);
 		return String.valueOf(result);
 	}
